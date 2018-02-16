681373de836155af98e4baa9a07ee227fa6ea10d
Mathias Lantean <mathias.lantean@innuy.com>
2018-02-16 15:51:36 -0300

    Add custom login html
    Added settings to find static filds and templates.
    Added auth_views urls.

--- innuy_lambda/settings/base.py
+++ innuy_lambda/settings/base.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core'
+    'core.apps.CoreConfig'
 ]
 
 MIDDLEWARE = [
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'innuy_lambda.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,6 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 # change default user model
 AUTH_USER_MODEL = 'core.User'
--- innuy_lambda/urls.py
+++ innuy_lambda/urls.py
@@ -18,7 +18,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 
+from django.contrib.auth import views as auth_views
+
 urlpatterns = [
+    path('login/', auth_views.login, name='login'),
+    path('logout/', auth_views.logout, name='logout'),
     path('admin/', admin.site.urls),
 ]
 
