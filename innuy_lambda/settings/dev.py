80885c5701536bab4ec23e68c544c0c7d415b0da
Nico <n.perezgiorgi@gmail.com>
2018-02-19 13:53:00 -0300

    Update settings

--- innuy_lambda/settings/dev.py
+++ innuy_lambda/settings/dev.py
@@ -2,6 +2,8 @@ from .base import *
 
 INSTALLED_APPS += [
     'debug_toolbar',
+    'zappa_django_utils',
+    'storages',
 ]
 
 
@@ -12,3 +14,21 @@ MIDDLEWARE += [
 INTERNAL_IPS = [
     '127.0.0.1',
 ]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'zappa_django_utils.db.backends.s3sqlite',
+        'NAME': 'sqlite.db',
+        'BUCKET': 'innuylambda'
+    }
+}
+
+ALLOWED_HOSTS = ['*']
+
+AWS_STORAGE_BUCKET_NAME = 'innuylambda-static'
+
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
+
+STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
